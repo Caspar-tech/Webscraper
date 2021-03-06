7d8f2cc75b782506da83277407c1104d005cfbea
Caspar <caspar.lombaers@gmail.com>
2021-03-06 16:53:17 +0100

    Checking txt but error in reponse.html.render()

--- Webscraper.py
+++ Webscraper.py
@@ -3,7 +3,14 @@ from requests_html import HTMLSession
 
 ### https://requests.readthedocs.io/projects/requests-html/en/latest/
 
+# BIG geldig maar geen HA
 BIG = 59919419601
+# BIG huisarts
+#BIG = 19059287401
+# BIG ongeldig
+#BIG = 59919419602
+
+BIG_list = [59919419601, 19059287401, 59919419602]
 
 url = ("https://zoeken.bigregister.nl/bignummer/%s" % BIG)
 
@@ -12,7 +19,18 @@ response = session.get(url)
 
 response.html.render()
 
-print(response.html.text)
+text = response.html.text
+print(text)
 
-# (reponse.html.search("Naam"))
+Check_BIG_Naam = text.find("Naam")
+#print("BIG_naam: " + str(Check_BIG_Naam))
+Check_BIG_Huisartsgeneeskunde = text.find("Huisartsgeneeskunde")
+
+if Check_BIG_Huisartsgeneeskunde > -1:
+    print("BIG is gekoppeld aan huisarts")
+elif Check_BIG_Naam > -1:
+    print("BIG is geldig, maar geen huisarts")
+else:
+    print("BIG levert geen resultaat op")
+
 
