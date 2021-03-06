d8b307c447999beac672bf8083dacf615c8f5604
Caspar <caspar.lombaers@gmail.com>
2021-03-06 16:17:41 +0100

    req.txt added

--- Webscraper.py
+++ Webscraper.py
@@ -3,7 +3,9 @@ from requests_html import HTMLSession
 
 ### https://requests.readthedocs.io/projects/requests-html/en/latest/
 
-url = "https://zoeken.bigregister.nl/bignummer/59919419601"
+BIG = 59919419601
+
+url = ("https://zoeken.bigregister.nl/bignummer/%s" % BIG)
 
 session = HTMLSession()
 response = session.get(url)
@@ -11,4 +13,6 @@ response = session.get(url)
 response.html.render()
 
 print(response.html.text)
+
+# (reponse.html.search("Naam"))
 
