8dea788cc101c35557de6d993711c3ea9e12684d
Caspar <caspar.lombaers@gmail.com>
2021-03-09 10:18:55 +0100

    Added explenations in code

--- Webscraper.py
+++ Webscraper.py
@@ -1,8 +1,6 @@
 import requests
 from requests_html import HTMLSession
 
-### https://requests.readthedocs.io/projects/requests-html/en/latest/
-
 # BIG geldig maar geen HA
 #BIG = 59919419601
 # BIG huisarts
@@ -12,27 +10,38 @@ from requests_html import HTMLSession
 
 BIG_list = [59919419601, 19059287401, 59919419602]
 
+# We loop trough all BIG-numbers in the list.
+# For every number we load the website and check the content
 for BIG in BIG_list:
-    print("https://zoeken.bigregister.nl/bignummer/%s" % str(BIG))
+    # We create the URL to load using the BIG-number from the list
     url = ("https://zoeken.bigregister.nl/bignummer/%s" % str(BIG))
 
+    # This code block uses HTMLsession
+    # https://requests.readthedocs.io/projects/requests-html/en/latest/
     session = HTMLSession()
     response = session.get(url)
+    # With adding "sleep" we wait a short while while the script is executed
+    # than after waiting we get the website as loaded
+    # without sleep it renders too fast and we get the website before the script is executed
+    response.html.render(sleep=0.1)
 
-    response.html.render(sleep=0.2)
-
+    # We place the rendered website in a variable called "text"
     text = response.html.text
-    print(text)
 
+    # We use parsing to searh the text
+    # only when a BIG-number is true the word "Naam" occurs
+    # only when a true Big-number belongs to a huisarts the word "Huisartsgeneeskunde" occurs
     Check_BIG_Naam = text.find("Naam")
-    #print("BIG_naam: " + str(Check_BIG_Naam))
     Check_BIG_Huisartsgeneeskunde = text.find("Huisartsgeneeskunde")
 
+    print(BIG)
     if Check_BIG_Huisartsgeneeskunde > -1:
         print("BIG is gekoppeld aan huisarts")
     elif Check_BIG_Naam > -1:
         print("BIG is geldig, maar geen huisarts")
     else:
         print("BIG levert geen resultaat op")
+
+    print("------------------------------------")
 
 
