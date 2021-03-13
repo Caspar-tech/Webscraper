d17c98409428f8e23a9c3e3e8f9626c5892e56f7
Caspar <caspar.lombaers@gmail.com>
2021-03-13 20:55:21 +0100

    Testing BIG and testing name work

--- Webscraper.py
+++ Webscraper.py
@@ -1,6 +1,7 @@
 from requests_html import HTMLSession
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
+import sys
 
 # Test BIG-numbers
 # BIG geldig maar geen HA
@@ -11,7 +12,13 @@ from openpyxl.styles import PatternFill
 #BIG = 59919419602
 
 # We use openpyxl to get and put data in an excel sheet
-wb = load_workbook("Testbig.xlsx")
+# The program wil fail if the excel file is not closed.
+# So I created a custom message for that situation with try/except.
+try:
+    wb = load_workbook("Testbig.xlsx")
+except:
+    print("ERROR: Something went wrong loading the excel file. Did your remember to close the excel file before using?")
+    sys.exit()
 ws = wb.active
 
 # We presume that the BIG-numbers are stored in column A of the excel sheet
@@ -37,7 +44,9 @@ for cell in col:
         # We place the rendered website in a variable called "text"
         text = response.html.text
 
-        # We use parsing to searh the text
+        ### ----------------------- Checking BIG ----------------------------- ###
+
+        # We use parsing to search the text
         # only when a BIG-number is true the word "Naam" occurs
         # only when a true Big-number belongs to a huisarts the word "Huisartsgeneeskunde" occurs
         # When the word does not occur it will produce "-1"
@@ -58,6 +67,45 @@ for cell in col:
         print(Outcome)
         # We store the outcome in the column (+2) the cell right of the BIG-number
         Two_cells_to_the_right = ws.cell(row=(cell.row), column=(cell.column + 2))
+        Two_cells_to_the_right.value = Outcome
+        # We change the color of the cell depending on the outcome
+        Two_cells_to_the_right.fill = PatternFill("solid", start_color=Cell_fill_color)
+
+        ### ----------------------- Checking last name ----------------------------- ###
+
+        # We take the name of the doctor from the excel (2nd row)
+        # With parsing we check whether this name is also found when searching for the BIG-number
+        Name_in_excel = ws.cell(row=(cell.row), column=(cell.column + 1)).value
+
+        # We search the text for the name of the doctor in the excel
+        # This search will give the "location" of the Name (around 230)
+        # if the name is not found it will return -1
+        Name_location = text.find(Name_in_excel)
+
+        # To be sure the name is exactly the same we check whether there is a space in front of it
+        # we use the location of where we found the name -1 place
+        # to prevent: search --> Pol and get positive on Repol
+        Space_before_name = text.find(" ", (Name_location - 1), Name_location)
+
+        # We also check if there is a "G" right after the name. This is the first letter on the next row.
+        # we use the location of the name + length of the name + 1
+        # To prevent: search --> Frank and get positive on Franken
+        Letters_in_name = len(Name_in_excel)
+        G_after_name = text.find("G", (Name_location + Letters_in_name + 1),
+                        (Name_location + Letters_in_name + 2))
+
+        # If one of these 3 checks is -1 we have a mismatch in the name
+        if Name_location < 0 or Space_before_name < 0 or G_after_name <0:
+            print("Naam komt niet overeen")
+            Outcome = "Naam komt niet overeen"
+            Cell_fill_color = "FF0000"  # Red
+        else:
+            print("Naam komt overeen")
+            Outcome = "Naam komt overeen"
+            Cell_fill_color = "99CC00"  # Green
+
+        # We store the outcome in the column (+2) the cell right of the Name
+        Two_cells_to_the_right = ws.cell(row=(cell.row), column=(cell.column + 3))
         Two_cells_to_the_right.value = Outcome
         # We change the color of the cell depending on the outcome
         Two_cells_to_the_right.fill = PatternFill("solid", start_color=Cell_fill_color)
